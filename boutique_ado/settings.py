84a2cbff27f6aec3a3da21b86c4ac02bf7b68260
KevinFlanagan <flanagank777@gmail.com>
2024-07-25 14:26:47 +0100

    Initial commit

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n0=2qrk%d5^r@afm1rtl0o(_^g)0rky#gx_jw4fu^-emv(celx'
+SECRET_KEY = 'django-insecure-_^=0aigwpocrq%zs)6umo&j=araw*jj1)m+esc$e9w_b+jrm1i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
