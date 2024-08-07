9510dc4c38b299158157a2d7ec433b904890fbcb
KevinFlanagan <flanagank777@gmail.com>
2024-08-07 15:51:25 +0100

    Add checkout virews and templates.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    #Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boutique_ado.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
